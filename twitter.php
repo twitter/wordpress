aad18a4cb54d54714d5343d34ae6e396b27804a0
Eric King <eric@webdeveric.com>
2015-02-25 22:28:54 -0600

    Added admin notice for the user if the PHP version requirements are not met

--- twitter.php
+++ twitter.php
@@ -22,48 +22,14 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-/**
- * @package twitter
- * @version 1.0.0
- */
-/*
-Plugin Name: Twitter
-Plugin URI:  http://wordpress.org/plugins/twitter/
-Description: Official Twitter plugin for WordPress. Embed Twitter content and grow your audience on Twitter.
-Version:     1.0.0
-Author:      Twitter
-Author URI:  https://dev.twitter.com/
-License:     MIT
-Text Domain: twitter
-Domain Path: /languages/
-*/
-
-
-// make sure the plugin does not expose any info if called directly
-if ( ! function_exists( 'add_action' ) ) {
-	if ( ! headers_sent() ) {
-		if ( function_exists( 'http_response_code' ) ) {
-			http_response_code( 403 );
-		} else {
-			header( 'HTTP/1.1 403 Forbidden', true, 403 );
-		}
-	}
-	exit( 'Hi there! I am a WordPress plugin requiring functions included with WordPress. I am not meant to be addressed directly.' );
-}
-
-// plugin requires PHP 5.4 or newer
-if ( version_compare( PHP_VERSION, '5.4.0', '<' ) ) {
-	trigger_error( 'The Twitter plugin for WordPress requires PHP version 5.4 or higher.' );
-	return;
-}
 
 // PHP namespace autoloader
 require_once( dirname( __FILE__ ) . '/autoload.php' );
 
 // initialize on plugins loaded
 add_action(
-	'plugins_loaded',
-	array( '\\Twitter\\WordPress\\PluginLoader', 'init' ),
-	0, // priority
-	0 // expected arguments
+    'plugins_loaded',
+    array( '\\Twitter\\WordPress\\PluginLoader', 'init' ),
+    0, // priority
+    0 // expected arguments
 );
