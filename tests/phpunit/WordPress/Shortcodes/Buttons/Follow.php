99190876e81bc6f837c8c34551a8e445a45fc014
Niall Kennedy <niall@niallkennedy.com>
2017-01-09 20:42:39 -0800

    PSR style spacing

--- tests/phpunit/WordPress/Shortcodes/Buttons/Follow.php
+++ tests/phpunit/WordPress/Shortcodes/Buttons/Follow.php
@@ -29,100 +29,106 @@ namespace Twitter\Tests\WordPress\Shortcodes\Buttons;
  * @group shortcode
  * @coversDefaultClass \Twitter\WordPress\Shortcodes\Buttons\Follow
  */
-final class Follow extends \WP_UnitTestCase {
+final class Follow extends \WP_UnitTestCase
+{
 
-	/**
-	 * Test extracting a Twitter screen name from a shortcode through screen_name attribute
-	 *
-	 * @since 2.0.0
-	 *
-	 * @covers ::getShortcodeAttributes
-	 * @small
-	 *
-	 * @dataProvider screenNameProvider
-	 *
-	 * @param string $screen_name Twitter screen name test value
-	 *
-	 * @return void
-	 */
-	public function testGetShortcodeAttributesScreenName( $screen_name ) {
-		$expected = 'TwitterDev';
+    /**
+     * Test extracting a Twitter screen name from a shortcode through screen_name attribute
+     *
+     * @since 2.0.0
+     *
+     * @covers ::getShortcodeAttributes
+     * @small
+     *
+     * @dataProvider screenNameProvider
+     *
+     * @param string $screen_name Twitter screen name test value
+     *
+     * @return void
+     */
+    public function testGetShortcodeAttributesScreenName($screen_name)
+    {
+        $expected = 'TwitterDev';
 
-		$options = \Twitter\WordPress\Shortcodes\Buttons\Follow::getShortcodeAttributes( array( 'screen_name' => $screen_name ) );
-		$this->assertTrue( ( isset( $options['screen_name'] ) && $options['screen_name'] === $expected ), 'Failed to extract screen name from attribute' );
-	}
+        $options = \Twitter\WordPress\Shortcodes\Buttons\Follow::getShortcodeAttributes(array( 'screen_name' => $screen_name ));
+        $this->assertTrue(( isset($options['screen_name']) && $options['screen_name'] === $expected ), 'Failed to extract screen name from attribute');
+    }
 
-	/**
-	 * Twitter screen name provider
-	 *
-	 * @since 2.0.0
-	 *
-	 * @return array array of arrays of Twitter screen names
-	 */
-	public static function screenNameProvider() {
-		return array(
-			array( 'TwitterDev' ),
-			array( '@TwitterDev' ),
-			array( ' TwitterDev ' ),
-		);
-	}
+    /**
+     * Twitter screen name provider
+     *
+     * @since 2.0.0
+     *
+     * @return array array of arrays of Twitter screen names
+     */
+    public static function screenNameProvider()
+    {
+        return array(
+            array( 'TwitterDev' ),
+            array( '@TwitterDev' ),
+            array( ' TwitterDev ' ),
+        );
+    }
 
-	/**
-	 * Test hiding Tweet counts through a shortcode attribute value
-	 *
-	 * @since 2.0.0
-	 *
-	 * @covers ::getShortcodeAttributes
-	 * @small
-	 *
-	 * @dataProvider falseyShortcodeParameterProvider
-	 *
-	 * @param bool|int|string $truthy_value truthy value to test
-	 *
-	 * @return void
-	 */
-	public function testGetShortcodeAttributesShowCount( $truthy_value ) {
-		$options = \Twitter\WordPress\Shortcodes\Buttons\Follow::getShortcodeAttributes( array( 'show_count' => $truthy_value ) );
-		$this->assertTrue( ( isset( $options['show_count'] ) && false === $options['show_count'] ), 'Failed to enable follow count from attribute' );
-	}
+    /**
+     * Test hiding Tweet counts through a shortcode attribute value
+     *
+     * @since 2.0.0
+     *
+     * @covers ::getShortcodeAttributes
+     * @small
+     *
+     * @dataProvider falseyShortcodeParameterProvider
+     *
+     * @param bool|int|string $truthy_value truthy value to test
+     *
+     * @return void
+     */
+    public function testGetShortcodeAttributesShowCount($truthy_value)
+    {
+        $options = \Twitter\WordPress\Shortcodes\Buttons\Follow::getShortcodeAttributes(array( 'show_count' => $truthy_value ));
+        $this->assertTrue(( isset($options['show_count']) && false === $options['show_count'] ), 'Failed to enable follow count from attribute');
+    }
 
-	/**
-	 * Test hiding Twitter screen name through a shortcode attribute value
-	 *
-	 * @since 2.0.0
-	 *
-	 * @covers ::getShortcodeAttributes
-	 * @small
-	 *
-	 * @dataProvider falseyShortcodeParameterProvider
-	 *
-	 * @param bool|int|string $falsey_value falsey value to test
-	 *
-	 * @return void
-	 */
-	public function testGetShortcodeAttributes( $falsey_value ) {
-		$options = \Twitter\WordPress\Shortcodes\Buttons\Follow::getShortcodeAttributes( array( 'show_screen_name' => $falsey_value ) );
-		$this->assertTrue( ( isset( $options['show_screen_name'] ) && false === $options['show_screen_name'] ), 'Failed to disable screen name display from attribute' );
-	}
+    /**
+     * Test hiding Twitter screen name through a shortcode attribute value
+     *
+     * @since 2.0.0
+     *
+     * @covers ::getShortcodeAttributes
+     * @small
+     *
+     * @dataProvider falseyShortcodeParameterProvider
+     *
+     * @param bool|int|string $falsey_value falsey value to test
+     *
+     * @return void
+     */
+    public function testGetShortcodeAttributes($falsey_value)
+    {
+        $options = \Twitter\WordPress\Shortcodes\Buttons\Follow::getShortcodeAttributes(array( 'show_screen_name' => $falsey_value ));
+        $this->assertTrue(( isset($options['show_screen_name']) && false === $options['show_screen_name'] ), 'Failed to disable screen name display from attribute');
+    }
 
-	/**
-	 * Show count falsey value provider
-	 *
-	 * @since 2.0.0
-	 *
-	 * @return array array of shortcode falsey values
-	 */
-	public static function falseyShortcodeParameterProvider() {
-		return array(
-			array( false ),
-			array( 0 ),
-			array( '0' ),
-			array( 'false' ),
-			array( 'FALSE' ),
-			array( 'no' ),
-			array( 'NO' ),
-			array( 'off' ),
-			array( 'OFF' ),
-		);
-	}
+    /**
+     * Show count falsey value provider
+     *
+     * @since 2.0.0
+     *
+     * @return array array of shortcode falsey values
+     */
+    public static function falseyShortcodeParameterProvider()
+    {
+        return array(
+            array( false ),
+            array( 0 ),
+            array( '0' ),
+            array( 'false' ),
+            array( 'FALSE' ),
+            array( 'no' ),
+            array( 'NO' ),
+            array( 'off' ),
+            array( 'OFF' ),
+        );
+    }
 }
